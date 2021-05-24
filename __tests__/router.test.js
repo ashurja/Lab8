903349637cb5ec1449c5ecb4c92f300cc675afb0
Jamshed <jamshed00ashurov@gmail.com>
2021-05-24 12:39:16 -0700

    Fix

--- __tests__/router.test.js
+++ __tests__/router.test.js
@@ -2,3 +2,26 @@
  * @jest-environment jsdom
  */
 
+ import { TestScheduler } from '@jest/core'
+ import {pushToHistory} from '../scripts/router.js'
+ 
+ describe('Testing length and state of the history function', () => {
+     test('testing settings', () => {
+         let settings = pushToHistory('settings', 0); 
+         expect(settings.length).toBe(history.length); 
+         expect(settings.state).toBe(history.state); 
+     });
+ 
+     test('testing entry', () => {
+         let entry = pushToHistory('entry', 5); 
+         expect(entry.length).toBe(history.length); 
+         expect(entry.state).toBe(history.state); 
+     });
+ 
+     test('testing default', () => {
+         let home_page = pushToHistory('home_page', 3); 
+         expect(home_page.length).toBe(history.length); 
+         expect(home_page.state).toBe(history.state); 
+     });
+ });
+
